e0dc85c21a63f032be2c74e16ad8ebe7b2dcfa44
1Abc342PedroGina-novo <rmagalhaesjuridico5@gmail.com>
2026-09-24 15:31:07 -0300

    Expand global parameters and descriptors to 64-bits

--- rtl/ao486/global_regs.v
+++ rtl/ao486/global_regs.v
@@ -1,27 +1,24 @@
+/* Copyright (C) 2026,  Pedro Emanuel
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *   You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 /*
  * Copyright (c) 2014, Aleksander Osman
  * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * 
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 `include "defines.v"
@@ -30,20 +27,21 @@ module global_regs(
     input               clk,
     input               rst_n,
     
+    // Entradas expandidas para 64-bits
     input               glob_param_1_set,
-    input       [31:0]  glob_param_1_value,
+    input       [63:0]  glob_param_1_value,
     
     input               glob_param_2_set,
-    input       [31:0]  glob_param_2_value,
+    input       [63:0]  glob_param_2_value,
     
     input               glob_param_3_set,
-    input       [31:0]  glob_param_3_value,
+    input       [63:0]  glob_param_3_value,
     
     input               glob_param_4_set,
-    input       [31:0]  glob_param_4_value,
+    input       [63:0]  glob_param_4_value,
     
     input               glob_param_5_set,
-    input       [31:0]  glob_param_5_value,
+    input       [63:0]  glob_param_5_value,
     
     input               glob_descriptor_set,
     input       [63:0]  glob_descriptor_value,
@@ -51,17 +49,18 @@ module global_regs(
     input               glob_descriptor_2_set,
     input       [63:0]  glob_descriptor_2_value,
     
-    //output
-    output reg  [31:0]  glob_param_1,
-    output reg  [31:0]  glob_param_2,
-    output reg  [31:0]  glob_param_3,
-    output reg  [31:0]  glob_param_4,
-    output reg  [31:0]  glob_param_5,
+    // Saídas de registradores expandidas para 64-bits
+    output reg  [63:0]  glob_param_1,
+    output reg  [63:0]  glob_param_2,
+    output reg  [63:0]  glob_param_3,
+    output reg  [63:0]  glob_param_4,
+    output reg  [63:0]  glob_param_5,
 
     output reg  [63:0]  glob_descriptor,
     output reg  [63:0]  glob_descriptor_2,
     
-    output      [31:0]  glob_desc_base,
+    // Base de endereço expandida para 64-bits para suportar endereçamento x86_64
+    output      [63:0]  glob_desc_base,
     output      [31:0]  glob_desc_limit,
     output      [31:0]  glob_desc_2_limit
 );
@@ -69,35 +68,38 @@ module global_regs(
 //------------------------------------------------------------------------------
 
 assign glob_desc_limit = glob_descriptor[`DESC_BIT_G]? { glob_descriptor[51:48], glob_descriptor[15:0], 12'hFFF } : { 12'd0, glob_descriptor[51:48], glob_descriptor[15:0] };
-assign glob_desc_base  = { glob_descriptor[63:56], glob_descriptor[39:16] };
+
+// Extensão zero-padded temporária para 64 bits da base do descritor herdado de 32-bits.
+// Nota técnica de evolução: Conforme avançarmos para os descritores de sistema do Long Mode (128-bits), 
+// este trecho absorverá a parte alta vinda de 'glob_descriptor_2'.
+assign glob_desc_base  = { 32'd0, glob_descriptor[63:56], glob_descriptor[39:16] };
 
 assign glob_desc_2_limit = glob_descriptor_2[`DESC_BIT_G]? { glob_descriptor_2[51:48], glob_descriptor_2[15:0], 12'hFFF } : { 12'd0, glob_descriptor_2[51:48], glob_descriptor_2[15:0] };
-
 
 //------------------------------------------------------------------------------
 
 always @(posedge clk or negedge rst_n) begin
-    if(rst_n == 1'b0)           glob_param_1 <= 32'd0;
+    if(rst_n == 1'b0)           glob_param_1 <= 64'd0;
     else if(glob_param_1_set)   glob_param_1 <= glob_param_1_value;
 end
 
 always @(posedge clk or negedge rst_n) begin
-    if(rst_n == 1'b0)           glob_param_2 <= 32'd0;
+    if(rst_n == 1'b0)           glob_param_2 <= 64'd0;
     else if(glob_param_2_set)   glob_param_2 <= glob_param_2_value;
 end
 
 always @(posedge clk or negedge rst_n) begin
-    if(rst_n == 1'b0)           glob_param_3 <= 32'd0;
+    if(rst_n == 1'b0)           glob_param_3 <= 64'd0;
     else if(glob_param_3_set)   glob_param_3 <= glob_param_3_value;
 end
 
 always @(posedge clk or negedge rst_n) begin
-    if(rst_n == 1'b0)           glob_param_4 <= 32'd0;
+    if(rst_n == 1'b0)           glob_param_4 <= 64'd0;
     else if(glob_param_4_set)   glob_param_4 <= glob_param_4_value;
 end
 
 always @(posedge clk or negedge rst_n) begin
-    if(rst_n == 1'b0)           glob_param_5 <= 32'd0;
+    if(rst_n == 1'b0)           glob_param_5 <= 64'd0;
     else if(glob_param_5_set)   glob_param_5 <= glob_param_5_value;
 end
 
@@ -114,3 +116,4 @@ end
 //------------------------------------------------------------------------------
 
 endmodule
+     
